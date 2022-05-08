051b6a5f8c9c1076677b5b668e5586c689ff074f
yakov103 <yaakov103@gmail.com>
2022-05-08 07:40:52 -0700

    game.hpp is ok

--- sources/Player.hpp
+++ sources/Player.hpp
@@ -5,10 +5,40 @@ using namespace std;
 
 namespace coup{
 
+enum LastAction{
+    none,
+    income,
+    foreign_aid,
+    coup,
+    tax,
+    assassinate,
+    transfer,
+    block, 
+    steal
 
+};
+class Game ; 
 class Player{
    public : 
-   bool isAlive; 
+    string name ; 
+    LastAction act ;
+    int amountCoins; 
+    bool isAlive; 
+
+    Player(Game &game, string name);//constructor
+    ~Player();// destructor
+    int coins(); //returns the amount of coins the player has
+    void income();//income action
+    void foreign_aid();//foreign aid
+    void coup(Player &player);//coup on player
+    virtual string role();//returns the role of the player
+    virtual void blocked();//blocked by another player
+    void startTurn(); //starts the turn
+    void endTurn(LastAction act);//end turn
+
+    
+
+
 };
 
 
